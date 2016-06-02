391abcaca27c9b66af0cb021cc772c94ad257f62
Kent Primrose <krose@solsoft.net>
2016-06-02 16:25:26 -0500

    Refactored tests

--- golog_test.go
+++ golog_test.go
@@ -11,17 +11,15 @@ import (
 	"testing"
 )
 
-type MockHandler struct{}
-
-func (mh MockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Output for URL: %s", r.URL.Path)
-}
-
 func TestMockHandler(t *testing.T) {
 	var mockWriter bytes.Buffer
 	mockLogger := log.New(&mockWriter, "", log.Ldate|log.Ltime)
 
-	handler := MockHandler{}
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/text")
+		fmt.Fprintf(w, "Output for URL: %s", r.URL.Path)
+	})
 	server := httptest.NewServer(Handler(handler, mockLogger))
 	defer server.Close()
 
@@ -55,5 +53,35 @@ func TestMockHandler(t *testing.T) {
 	received = mockWriter.String()
 	if !r.MatchString(received) {
 		t.Errorf("Logged '%s' doesn't match '%s'", received, m)
+	}
+}
+
+// Needed for test coverage
+func TestNormalHandler(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Output for URL: %s", r.URL.Path)
+	})
+	server := httptest.NewServer(Handler(handler))
+	defer server.Close()
+
+	resp, err := http.Get(server.URL)
+
+	if err != nil {
+		t.Fatalf("http.Get error: %s", err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Received non-200 response: %d", resp.StatusCode)
+	}
+
+	actual, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll error: %s", err.Error())
+	}
+
+	expected := "Output for URL: /"
+	received := string(actual)
+	if received != expected {
+		t.Errorf("Expected '%s' but received '%s'", expected, received)
 	}
 }
